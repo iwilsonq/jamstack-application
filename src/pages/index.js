6a8458c22120d420a3f154dbc40fb2b58ebd60d0
Ian Wilson <iwilsonq@gmail.com>
2018-10-05 16:28:01 -0700

    lets go

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Ian Wilson</h1>
+    <p>I've got two tickets to paradise.</p>
+    <p>Pack your bags, we'll leave tonight.</p>
+    <br />
+
+    <p>For the 2018 JAMStack hackathon on November 3-4 at GitHub.</p>
+    <small>I don't actually have two tickets. I would like one though.</small>
+  </Layout>
+)
+
+export default IndexPage
